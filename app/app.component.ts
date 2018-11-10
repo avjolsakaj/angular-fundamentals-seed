ce021096ddf1dbc7e071de3bb4247e6bd926657e
Avjol Sakaj <avjolsakaj@outlook.com>
2018-11-10 12:30:06 +0100

    Some changes

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,14 +1,37 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  styleUrls: ['app.component.scss'],
+  selector: "app-root",
+  styleUrls: ["app.component.scss"],
   template: `
     <div class="app">
-      Hello!
+      <input type="text" [value]="name" (input)="handleChange($event.target.value)">
+      <div *ngIf='name.length > 2'> 
+        Searching fro... {{ name }} 
+      </div>
+      <template [ngIf]='name.length > 2'>
+        <div> 
+          For... {{ name }} 
+        </div>
+      </template>
     </div>
   `
 })
 export class AppComponent {
+  title: string;
+  name: string = "Avjol";
+  logo: string = "img/logo.png";
+  numberOne: number = 1;
+  numberTwo: number = 2;
+  constructor() {
+    this.title = "Ultimate Angular";
+  }
 
+  handleChange(value: string) {
+    this.name = value;
+  }
+
+  handleClick(value: string) {
+    this.name = "Avjol Sakaj";
+  }
 }
